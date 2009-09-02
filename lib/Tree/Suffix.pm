59e1c0699dc1f9819641aac0c945f1b8af724269
gray <gray@cpan.org>
2009-09-02 12:30:06 -0700

    update copyright dates; remove custom ilbrary instructions for now

--- lib/Tree/Suffix.pm
+++ lib/Tree/Suffix.pm
@@ -219,7 +219,7 @@ L<http://search.cpan.org/dist/Tree-Suffix>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2006-2007 gray <gray at cpan.org>, all rights reserved.
+Copyright (C) 2006-2009 gray <gray at cpan.org>, all rights reserved.
 
 This library is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
